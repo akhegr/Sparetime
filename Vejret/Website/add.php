1f0cc7cd2573d6493a188a3e175bfd7960c5abc4
Aksel H. Gregersen <akhegr@gmail.com>
2022-03-22 18:26:01 +0100

    Update method to push weather data

--- Vejret/Website/add.php
+++ Vejret/Website/add.php
@@ -2,14 +2,15 @@
 	$key = $_GET["key"];
 	$angle = $_GET["dir"];
 	$windSpeed = $_GET["speed"];
-	$rainAmount = $_GET["amount"];
+	$rainAmount = $_GET["amount"]; //current total rain
 	$temp = $_GET["temp"];
+	$pres = $_GET["pres"];
 
     if($key!="D9ef") {
         echo("Error: The key isn't entered, or isn't correct.");
     }
 	elseif($windDir < 0) {
-		echo("Error: Windspeed is incorrect!");
+		echo("Error: Wind direction is incorrect!");
 	}
 	elseif($windSpeed < 0) {
 		echo("Error: Windspeed is incorrect!");
@@ -17,8 +18,11 @@
 	elseif($rainAmount < 0) {
 		echo("Error: Rain amount is incorrect!");
 	}
+	elseif(($pres < 940) OR ($pres > 1050)) {
+		echo("Error: Pressure is incorrect!");
+	}
 	else {		//http://passwordsgenerator.net 
-		$con=mysqli_connect("mysql17.unoeuro.com","akselhg_dk","deluxscans770","akselhg_dk_db") or die("SQL-login error");
+		$con=mysqli_connect("mysql.myserver.com","myUser","myPassword","myDb") or die("SQL-login error");
 		// Check connection
 		if (mysqli_connect_errno()) {
 			echo "Failed to connect to MySQL: " . mysqli_connect_error();
@@ -69,7 +73,19 @@
 	       		break;
 		}
 		
-		$result = mysqli_query($con, "INSERT INTO newWeather (datetime, windDir, windSpeed, rainAmount, temperature)  VALUES ('$time', '$windDir', '$windSpeed', '$rainAmount', '$temp')") or trigger_error("Query Failed! SQL: $sql - Error: ".mysqli_error(), E_USER_ERROR); //or die(mysqli_error($db));
+		$jsonfile = file_get_contents("http://api.akselhg.dk/weather/readOneWeather.php");
+		$json = json_decode($jsonfile);
+		$prevTotalRain =	($json->totalRain);
+		
+		if($prevTotalRain > $rainAmount)
+		{
+			$prevTotalRain = 0;
+		}
+		
+		$totalRain = $rainAmount;
+		$rainAmount = $rainAmount - $prevTotalRain;
+		
+		$result = mysqli_query($con, "INSERT INTO newWeather (datetime, windDir, windSpeed, rainAmount, temperature, totalRain, pressure)  VALUES ('$time', '$windDir', '$windSpeed', '$rainAmount', '$temp', '$totalRain', '$pres')") or trigger_error("Query Failed! SQL: $sql - Error: ".mysqli_error(), E_USER_ERROR); //or die(mysqli_error($db));
 		if($result)
 		{
 			echo ("The entered key is correct, and the data is now added to the database.");
